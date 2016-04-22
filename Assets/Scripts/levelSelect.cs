d08d42c6057dca159bbc17eca8c3d24188e86b60
jeduhaapajarvi_lk9 <hai.media@jedu.fi>
2016-04-22 09:34:23 +0300

    Sqlite-kokeilua (ei toimi vielä)

--- Assets/Scripts/levelSelect.cs
+++ Assets/Scripts/levelSelect.cs
@@ -2,6 +2,11 @@
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using System.Collections;
+// tietokanta
+using Mono.Data.Sqlite; 
+using System.Data; 
+using System;
+
 
 public class levelSelect : MonoBehaviour {
 
@@ -9,22 +14,53 @@ public class levelSelect : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		readDB ();
 		readStars ();
 	}
 
 
 	void readStars() {
 		// luetaan jo kerätyt tähdet muistista
-		if (PlayerPrefs.HasKey ("collected_stars")) {
-			int collected_stars = PlayerPrefs.GetInt ("collected_stars");
-
-			starsText.text = collected_stars.ToString ();
-		} else
-			starsText.text = "0";
+//		if (PlayerPrefs.HasKey ("collected_stars")) {
+//			int collected_stars = PlayerPrefs.GetInt ("collected_stars");
+//
+//			starsText.text = collected_stars.ToString ();
+//		} else
+//			starsText.text = "0";
 	}
 
 
 	public void StartLevel(int level) {
 		SceneManager.LoadScene (level);
 	}
+
+
+	void readDB() {
+		string conn = "URI=file:" + Application.dataPath + "/Pupukanta.s3db"; //Path to database.
+		Debug.Log("polku " + conn);
+
+		IDbConnection dbconn;
+		dbconn = (IDbConnection) new SqliteConnection(conn);
+		dbconn.Open(); //Open connection to the database.
+		if(dbconn == null) Debug.Log("yhteyttä ei ole");
+		IDbCommand dbcmd = dbconn.CreateCommand();
+		if (dbcmd == null)
+			Debug.Log ("komentoa ei ole");
+		string sqlQuery = "SELECT hyvis FROM Tasot";
+		dbcmd.CommandText = sqlQuery;
+		IDataReader reader = dbcmd.ExecuteReader();
+		while (reader.Read())
+		{
+			int value = reader.GetInt32(0);
+
+			Debug.Log( "value= "+value);
+		}
+		reader.Close();
+		reader = null;
+		dbcmd.Dispose();
+		dbcmd = null;
+		dbconn.Close();
+		dbconn = null;
+	}
+
 }
